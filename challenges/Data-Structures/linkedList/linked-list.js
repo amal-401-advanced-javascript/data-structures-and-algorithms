48d58500d69b8b32bd442fdaabab0d5b66d2b6cf
your username <amalalmomani19@gmail.com>
2020-06-01 02:18:39 +0300

    insertions-linked-list

--- challenges/Data-Structures/linkedList/linked-list.js
+++ challenges/Data-Structures/linkedList/linked-list.js
@@ -53,7 +53,80 @@ class SinglyLinkedList {
     }
     return str + ' -> NULL ';
   }
+
+  append(value){
+    let addNodeToEnd = new Node(value);
+
+    if(!this.head){
+      this.head = addNodeToEnd;
+      return this;
+    }
+    let current = this.head;
+    while(current.next){
+      current = current.next;
+    }
+    current.next = addNodeToEnd;
+    return this;
+  }
+  insertBefore(value, newVal){
+    let foundVal = false;
+    let current = this.head;
+    if(value === current.value){
+      let addBeforeFirstNode = new Node(newVal);
+      addBeforeFirstNode.next = this.head;
+      this.head = addBeforeFirstNode;
+    
+    }else{
+      while(current.next !=null && !foundVal){
+        if( value === current.next.value){
+          let addBeforeFirstNode = new Node(newVal);
+          addBeforeFirstNode.next = current.next;
+          current.next = addBeforeFirstNode;
+          foundVal = true;
+        }else{
+          current = current.next;
+        }
+        
+      }
+      return this;
+    }
+  }
+  
+  insertAfter(value, newVal){
+    
+    let foundVal = false;
+    let current = this.head;
+    
+    if(value === current.value){
+      this.append(newVal);
+    }else { 
+      while(current && ! foundVal){
+        if(value == current.value){
+          let addAfterFirstNode = new Node(newVal);
+          addAfterFirstNode.next = current.next;
+          current.next = addAfterFirstNode;
+          foundVal = true;
+        }else{
+          current = current.next;
+        }
+      }
+
+    }
+    return this;
+
+  }
+  
 }
+
+// let linkedListInsertions = new SinglyLinkedList();
+
+// linkedListInsertions.append(1);
+// linkedListInsertions.append(2);
+// linkedListInsertions.append(2);
+// // linkedListInsertions.append(4);
+
+// console.log(linkedListInsertions.insertAfter(4,5));
+// console.log(linkedListInsertions.toString());
 
 module.exports.SinglyLinkedList = SinglyLinkedList;
   
